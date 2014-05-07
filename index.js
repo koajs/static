f49bffa26b959c702e572f0b07cec50151f41c8b
TJ Holowaychuk <tj@vision-media.ca>
2014-05-07 08:39:39 -0700

    Revert "avoid using reserved word: static"
    This reverts commit 59b1fac910a62c39ff24d5b6c9fe2d736d5f012f.

--- index.js
+++ index.js
@@ -33,13 +33,13 @@ function serve(root, opts) {
   opts.index = opts.index || 'index.html';
 
   if (!opts.defer) {
-    return function *staticMiddleware(next){
+    return function *static(next){
       if (this.idempotent && (yield send(this, this.path, opts))) return;
       yield next;
     };
   }
 
-  return function *staticMiddleware(next){
+  return function *static(next){
     yield next;
 
     // response is already handled
