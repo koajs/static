a967ea7223a93c724dbd69a85a8ef23c0fa8160a
TJ Holowaychuk <tj@vision-media.ca>
2014-05-07 08:41:18 -0700

    change middleware function name to serve

--- index.js
+++ index.js
@@ -33,13 +33,13 @@ function serve(root, opts) {
   opts.index = opts.index || 'index.html';
 
   if (!opts.defer) {
-    return function *static(next){
+    return function *serve(next){
       if (this.idempotent && (yield send(this, this.path, opts))) return;
       yield next;
     };
   }
 
-  return function *static(next){
+  return function *serve(next){
     yield next;
 
     // response is already handled
