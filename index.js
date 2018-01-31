456eac3e95e4c1e4aaca31346a62be15290f2383
x22x22 <wadeking@qq.com>
2018-01-31 21:28:10 +0800

    Add a prefix attribute
    Add a prefix attribute
    the url prefix you wish to add, default to undefined.

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-
-'use strict'
+'use strict';
 
 /**
  * Module dependencies.
  */
 
-const debug = require('debug')('koa-static')
-const { resolve } = require('path')
-const assert = require('assert')
-const send = require('koa-send')
+const debug = require('debug')('koa-static');
+const { resolve } = require('path');
+const assert = require('assert');
+const send = require('koa-send');
 
 /**
  * Expose `serve()`.
  */
 
-module.exports = serve
+module.exports = serve;
 
 /**
  * Serve static files from `root`.
@@ -25,49 +24,55 @@ module.exports = serve
  * @api public
  */
 
-function serve (root, opts) {
-  opts = Object.assign({}, opts)
+function serve(root, opts) {
+  opts = Object.assign({}, opts);
 
-  assert(root, 'root directory is required to serve files')
+  assert(root, 'root directory is required to serve files');
 
   // options
-  debug('static "%s" %j', root, opts)
-  opts.root = resolve(root)
-  if (opts.index !== false) opts.index = opts.index || 'index.html'
+  debug('static "%s" %j', root, opts);
+  opts.root = resolve(root);
+  if (opts.index !== false) opts.index = opts.index || 'index.html';
 
   if (!opts.defer) {
-    return async function serve (ctx, next) {
-      let done = false
+    return async function serve(ctx, next) {
+      let done = false;
 
       if (ctx.method === 'HEAD' || ctx.method === 'GET') {
         try {
-          done = await send(ctx, ctx.path, opts)
+          done = await send(ctx, ctx.path, opts);
         } catch (err) {
           if (err.status !== 404) {
-            throw err
+            throw err;
           }
         }
       }
 
       if (!done) {
-        await next()
+        await next();
       }
-    }
+    };
   }
 
-  return async function serve (ctx, next) {
-    await next()
+  return async function serve(ctx, next) {
+    await next();
 
-    if (ctx.method !== 'HEAD' && ctx.method !== 'GET') return
+    if (ctx.method !== 'HEAD' && ctx.method !== 'GET') return;
     // response is already handled
-    if (ctx.body != null || ctx.status !== 404) return // eslint-disable-line
+    if (ctx.body != null || ctx.status !== 404) return; // eslint-disable-line
 
     try {
-      await send(ctx, ctx.path, opts)
+      var path = ctx.path;
+      // prefix must be ASCII code
+      if (opts.prefix) {
+        var regPrefix = new RegExp('^' + opts.prefix);
+        path = path.replace(regPrefix, '');
+      }
+      await send(ctx, path, opts);
     } catch (err) {
       if (err.status !== 404) {
-        throw err
+        throw err;
       }
     }
-  }
+  };
 }
